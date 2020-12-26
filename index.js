4f233c120eaeca6c53e2cc46052234be32a0a17a
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2020-12-26 11:59:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let name = ['Lisa', 'Kaitlin', 'Jan'];
 let eventName = "surprise";
 function writeCards(name) {
 for (let i=0; i<name.length ;i++){
-  console.log(`Thank you, ${name[i]});
+  console.log(`Thank you, ${name[i]}`);
 }
 }
 writeCards(name);
