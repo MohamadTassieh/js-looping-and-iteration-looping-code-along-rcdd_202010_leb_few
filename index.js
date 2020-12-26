9560427a20af0172da0dc3ccb218153ed5f230a4
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2020-12-26 11:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
 let name = ["Lisa", "Kaitlin", "Jan"];
 let eventName = "surprise";
-function writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise') {
+function writeCards(name,eventName) {
 for (let i=0; i<name.length ;i++){
-  console.log(`Thank you, ${[i]}, for the wonderful ${surprise} gift!`);
+  console.log(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
 }
 }
 writeCards(name);
