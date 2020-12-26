577ff70e65e0b81de62fe5486d426e42299160b0
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2020-12-26 12:45:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Code your solutions in this file
-let name = ["Ada", "Brendan", "Ali"];
-let eventName = "birthday";
+let name = ["Lisa", "Kaitlin", "Jan"];
+let eventName = "surprise";
 function writeCards(name, eventName) {
+let array=[];
 for (let i=0; i<name.length ;i++){
-  console.log(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+  array.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
 }
-return name;
+return array;
 }
-writeCards(name);
+
